6d67a9a55faef5c3768d5b3d4af64a70572536b8
TieWay59 <tieway59@foxmail.com>
2020-12-09 18:56:27 +0800

    add all

--- lab1/main.py
+++ lab1/main.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 
 def load_file(filename, spliter='\t'):
+    '''
+    载入数据
+    '''
     data = []
     with open(filename) as fr:
         for line in fr.readlines():
@@ -13,10 +16,16 @@ def load_file(filename, spliter='\t'):
 
 
 def dist(a, b):
+    '''
+    计算二维平面欧式距离
+    '''
     return sqrt(sum(power(a - b, 2)))
 
 
 def rand_cent(data, k):
+    '''
+    对数据生成在范围内的k个随机质心
+    '''
     n = shape(data)[1]
     ct = mat(zeros((k, n)))
     for j in range(n):
@@ -26,34 +35,47 @@ def rand_cent(data, k):
     return ct
 
 
-def k_means(dt, k):
-    m = shape(dt)[0]
-    ca = mat(zeros((m, 1)))
-    ct = rand_cent(dt, k)
-    flag = True
+def c_means(dt, k):
+    '''
+    输入（坐标数据，划分数量）
+    返回（质心，划分方案）
+    '''
+    m = shape(dt)[0]            # 数据个数
+    ca = mat(zeros((m, 1)))     # 构造划分方案矩阵，每行一个元素，代表对应的点属于那个簇
+    ct = rand_cent(dt, k)       # 构造质心矩阵，k行每行两个元素，代表坐标。初始状态随机选择质心。
+    flag = True                 # 循环标志
 
     while flag:
         flag = False
-        for j in range(m):
+        for j in range(m):  # 遍历数据中所有坐标
+            # 定义最近的质心编号与最近质心的距离。
             min_i, min_d = 0, dist(ct[0, :], dt[j, :])
 
+            # 遍历所有质心，找到最近的质心编号
             for i in range(1, k):
                 d = dist(ct[i, :], dt[j, :])
                 if d < min_d:
                     min_i, min_d = i, d
 
+            # 如果和旧的划分方案不同，则更新当前点的划分方案，并激活循环标志
             if ca[j, 0] != min_i:
                 ca[j, 0] = min_i
                 flag = True
 
+        # 根据本轮的划分方案，重新计算每个簇的质心
         for cent in range(k):
             p = dt[nonzero(ca == cent)[0]]
             ct[cent, :] = mean(p, axis=0)
 
+    # 返回结果
     return ct, ca
 
 
 def make_plot(data, ct, ca):
+    '''
+    绘图函数，输入（坐标数据，质心，划分方案）
+    绘制过程不作为本次实验的重点
+    '''
     m = shape(ct)[0]
     fig = plt.figure(figsize=(8, 7), dpi=250)
     markers = ['s', 'o', '^', 'p', 'd', 'v', 'h', '>', '<']
@@ -78,14 +100,13 @@ def make_plot(data, ct, ca):
         s=200)
 
 
-def main():
-    dataset = mat(load_file('image-tryouts/dataset4.txt', spliter=','))
-    centroid, assignment = k_means(dataset, 7)
+def main(filename, c):
+    dataset = mat(load_file(filename, spliter=','))
+    centroid, assignment = c_means(dataset, c)
     print(centroid)
     make_plot(dataset, centroid, assignment)
-    # plt.savefig('fix.jpg', dpi=550)
     plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    main('datasets/dataset4.txt', 7)  # 选择数据集和划分数，运行c_means
